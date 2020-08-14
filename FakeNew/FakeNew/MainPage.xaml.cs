a71a072abbdc7dacea3acf90d9f2278658fea5ae
thangnm <thangnm@trunganhedukids.com>
2020-08-14 11:08:45 +0700

    fake new

---
+++ FakeNew/FakeNew/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using FakeNew.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FakeNew
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<NewsItem> NewsItems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NewsItems = new ObservableCollection<NewsItem>();
+        }
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+        private void ListBox_SelectionChanged(object sender,SelectionChangedEventArgs e)
+        {
+            if (Financial.IsSelected)
+            {
+                NewManager.GetNews("Financial", NewsItems);
+                TitleTextBlock.Text = "Financial";
+            }
+            else if (Food.IsSelected)
+            {
+                NewManager.GetNews("Food", NewsItems);
+                TitleTextBlock.Text = "Food";
+            }
+           
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Financial.IsSelected = true;
+        }
+    }
+}
---
+++ FakeNew/FakeNew/Model/NewsItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeNew.Model
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Headline { get; set; }
+        public string Subhead { get; set; }
+        public string DateLine { get; set; }
+        public string Image { get; set; }
+    }
+    public class NewManager
+    {
+        public static List<NewsItem> getNewsItems()
+        {
+            var items = new List<NewsItem>();
+            items.Add(new NewsItem() { Id = 1, Category = "Financial", Headline = "Lorem Ipsum", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Financial.png" });
+            items.Add(new NewsItem() { Id = 2, Category = "Financial", Headline = "Etiam ac felis viverra", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Financial2.png" });
+            items.Add(new NewsItem() { Id = 3, Category = "Financial", Headline = "Interger sed turpis erat", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Financia3.png" });
+            items.Add(new NewsItem() { Id = 4, Category = "Financial", Headline = "Proin sem neque", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Financial4.png" });
+            items.Add(new NewsItem() { Id = 5, Category = "Financial", Headline = "Mauris bibendum non leo vitea tempor", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Financial5.png" });
+            items.Add(new NewsItem() { Id = 6, Category = "Assets/Financial.png", Headline = "Mauris bibendum non leo vitea tempor", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Food1.png" });
+            items.Add(new NewsItem() { Id = 7, Category = "Food", Headline = "Mauris bibendum non leo vitea tempor", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Food2.png" });
+            items.Add(new NewsItem() { Id = 8, Category = "Food", Headline = "Mauris bibendum non leo vitea tempor", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Food3.png" });
+            items.Add(new NewsItem() { Id = 9, Category = "Food", Headline = "Mauris bibendum non leo vitea tempor", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Food4.png" });
+            items.Add(new NewsItem() { Id = 10, Category = "Food", Headline = "Mauris bibendum non leo vitea tempor", Subhead = "doro sit amet", DateLine = "Nunc tristique nec", Image = "Assets/Food5.png" });
+
+            return items;
+        }
+        public static void GetNews(string category, ObservableCollection<NewsItem> newsItems)
+        {
+            var allItems = getNewsItems();
+            var filteredNewsItems = allItems.Where(p => p.Category == category).ToList();
+            newsItems.Clear();
+            filteredNewsItems.ForEach(p => newsItems.Add(p));
+        }
+    }
+}
+
