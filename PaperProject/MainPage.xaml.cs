5e75aa459e433d85c0ccdd7426740f38b8e5415e
thangnm <thangnm@trunganhedukids.com>
2020-08-31 10:47:48 +0700

    projectnhom

---
+++ PaperProject/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using PaperProject.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using static PaperProject.Models.Paper;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PaperProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        public async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RootObject myPage = await Paper.GetPaper();
+                string image = String.Format(myPage.image);
+                ImageResult.Source = new BitmapImage(new Uri(image, UriKind.Absolute));
+                DateResult.Text = myPage.date;
+                TitleResult.Text = myPage.title;
+                string description = myPage.content.description;
+                ContentResult1.Text = description;
+
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ PaperProject/Models/Paper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperProject.Models
+{
+    public class Paper
+    {
+        public class Content
+        {
+            public string description { get; set; }
+            public string url { get; set; }
+        }
+
+        public class RootObject
+        {
+            public int id { get; set; }
+            public string date { get; set; }
+            public string title { get; set; }
+            public string image { get; set; }
+            public Content content { get; set; }
+        }
+        public async static Task<RootObject> GetPaper()
+        {
+            var http = new HttpClient();
+            var url = String.Format("http://api-demo-anhth.herokuapp.com/data.json");
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            //Khoi tao Stream local de doc json
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            //Doc object da phan tich duoc tu json
+            var data = (RootObject)serializer.ReadObject(ms);
+            return data;
+        }
+    }
+}
