beb8ffcbc537fce8b91632f00744e28cc5a8fae9
Nguyễn Minh Thắng <minhthang009@gmail.com>
2020-10-05 10:12:17 +0700

    practical

--- AlluringDecors/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlluringDecors.MvcApplication" Language="C#" %>
---
+++ Practical/Controllers/ContactsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Practical.Models;
+
+namespace Practical.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private PracticalEntities1 db = new PracticalEntities1();
+
+        // GET: Contacts
+        public ViewResult Index(string sortOrder, string search, string currentFilter, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            if (search != null)
+            {
+                page = 1; // nếu search có giá trị trả về page = 1
+            }
+            else
+            {
+                search = currentFilter;
+            }
+            ViewBag.CurrentFilter = search;
+            var contacts = from s in db.Contacts select s;
+            if (!String.IsNullOrEmpty(search)) // nếu search string có thì in ra hoặc không thì không in ra
+            {
+                contacts = contacts.Where(s => s.ContactName.Contains(search)); // contains là để check xem lastname hoặc firstName có chứa search string ở trên 
+
+            }
+            switch (sortOrder)
+            {
+                case "name desc":
+                    contacts = contacts.OrderByDescending(s => s.ContactName);
+                    break;
+
+                default:
+                    contacts = contacts.OrderBy(s => s.ContactName);
+                    break;
+            }
+            int pageSize = 3; //  khai báo số lượng record trên 1 page
+            int pageNumber = (page ?? 1); // page number là page đang chọn nếu không chọn sẽ = 1
+            return View(contacts.ToPagedList(pageNumber, pageSize));
+            /*      var students = db.Students.Include(s => s.Class);*/
+            /*   return View(students.ToList());*/
+        }
+
+        // GET: Contacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ContactName,ContactNumber,GroupName,Hiredate,Birthday")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,ContactName,ContactNumber,GroupName,Hiredate,Birthday")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
